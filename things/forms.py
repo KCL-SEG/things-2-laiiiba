c0294d1817961d26a7dff27a1775e7d48e894db2
Laiba Asif <laiba.asif@kcl.ac.uk>
2023-12-05 21:54:51 +0000

    create and handle form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,11 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
 
-# Create your forms here.
+class ThingForm(forms.ModelForm): 
+    """Form enabling unregistered users to create tasks for a team."""
+     
+    class Meta: 
+        model = Thing
+        fields = ['name', 'description', 'quantity'] 
+        widgets = {'description':forms.Textarea(), 'quantity':forms.NumberInput()}
--- things/views.py
+++ things/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ThingForm
+from .models import Thing
 
 def home(request):
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            pass
+    else:
+        form = ThingForm()
     return render(request, 'home.html', {'form': form})
